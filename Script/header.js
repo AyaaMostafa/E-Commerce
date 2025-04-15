c09aa60ec65f0e882f4940110fe0532be855a223
Tarek Samy Fathy aziz <tarek.4200248@eng.modern-academy.edu.eg>
2025-04-15 22:59:36 +0200

    add cart / fav

--- Script/header.js
+++ Script/header.js
@@ -5,6 +5,15 @@ document.body.addEventListener("click", function (e) {
         const urlParams = new URLSearchParams(window.location.search);
         const email = urlParams.get("email");
         console.log(e.target);
-        window.location.href = `../Pages/favorite/favorite.html?email=${email}`;
+        window.location.href = `../Pages/favorite/favourite.html?email=${email}`;
+    }
+});
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.id === "cart-icon") {
+        const urlParams = new URLSearchParams(window.location.search);
+        const email = urlParams.get("email");
+        console.log(e.target);
+        window.location.href = `../Pages/cart/cart.html?email=${email}`;
     }
 });
