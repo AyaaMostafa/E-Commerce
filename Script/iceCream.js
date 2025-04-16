2a2728222f9061b0ac01f6473d5fe7bb55162f9b
Ma-wardany <wardany.dev@gmail.com>
2025-04-16 20:38:00 +0200

    fix pull errors

--- Script/iceCream.js
+++ Script/iceCream.js
@@ -1,44 +1,14 @@
-import { createCard } from './../components/productCard.js';  
+import { fetchProducts, loadDataToHtml } from './fetchProducts.js'
 
-function loadIceCreamProducts() {
-    const iceCreamData = [
-        {
-            image: "/Resources/download (9).jpg",
-            title: "Spaghetti Bolognese",
-            price: 95,
-            description: "Classic spaghetti with rich meat sauce"
-        },
-        {
-            image: "/Resources/download (9).jpg",
-            title: "Fettuccine Alfredo",
-            price: 105,
-            description: "Creamy Alfredo sauce with fettuccine pasta"
-        },
-        {
-            image: "/Resources/download (9).jpg",
-            title: "Penne Arrabbiata",
-            price: 85,
-            description: "Penne pasta with spicy tomato sauce"
-        },
-        {
-            image: "/Resources/download (9).jpg",
-            title: "Lasagna",
-            price: 110,
-            description: "Layers of pasta, meat sauce, and cheese"
-        }
-    ];
 
-    const iceCreamProducts = document.getElementById("iceCreamProducts");
+let loadData = async () => {
+  const data = await fetchProducts("dessert", "iceCream");
+  console.log(data);
+  return data;
+};
 
-    if (iceCreamProducts) {
-        iceCreamProducts.innerHTML = "";  
-        iceCreamData.forEach(product => {
-            let productCard = createCard(product.title, product.price, product.image);
-            iceCreamProducts.appendChild(productCard);  
-        });
-    } else {
-        console.error("Products container not found");
-    }
-}
+loadData().then(res => {
+  console.log(res);
 
-loadIceCreamProducts();
+  loadDataToHtml(res.items);
+});
